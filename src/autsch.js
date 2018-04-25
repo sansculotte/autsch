6de0dba2829e5acbf8101298e7860103b46e9262
ub <u@sansculotte.net>
2018-04-25 20:01:05 +0200

    remove semicolons

--- src/autsch.js
+++ src/autsch.js
@@ -1,23 +1,23 @@
 function xmlHttpGet (URL, callback) {
-  var xhrRequest = new XMLHttpRequest();
+  var xhrRequest = new XMLHttpRequest()
 
-  xhrRequest.open('GET', URL, true);
+  xhrRequest.open('GET', URL, true)
   xhrRequest.onreadystatechange = function () {
     if(xhrRequest.readyState === 4) {
       if(xhrRequest.status === 200) {
-        callback(JSON.parse(xhrRequest.responseText));
+        callback(JSON.parse(xhrRequest.responseText))
       }
     }
-  };
-  xhrRequest.send();
+  }
+  xhrRequest.send()
 }
 function findIndex(check, arr) {
   for (var x=0; x<arr.length; x++) {
     if(check(arr[x])) {
-      return x;
+      return x
     }
   }
-  return -1;
+  return -1
 }
 var Player = {
   conf: {
@@ -32,97 +32,97 @@ var Player = {
     length: 0,
     position: null,
     current: function () {
-      return this.tracks[this.position];
+      return this.tracks[this.position]
     }
   },
   config: function (conf) {
     if (conf !== undefined) {
       Object.keys(this.conf).forEach(function (k) {
-        this.conf[k] = conf[k] || this.conf[k];
-      });
+        this.conf[k] = conf[k] || this.conf[k]
+      })
     }
   },
   init: function (audio, conf) {
 
-    this.config(conf);
+    this.config(conf)
 
-    var player   = this;
-    var playlist = this.playlist;
-    var conf     = this.conf;
+    var player   = this
+    var playlist = this.playlist
+    var conf     = this.conf
 
-    this.audio = audio;
-    this.playlist.url = audio.getAttribute('data-src');
-    this.playlist.el = document.createElement('ul');
+    this.audio = audio
+    this.playlist.url = audio.getAttribute('data-src')
+    this.playlist.el = document.createElement('ul')
 
     this.loadPlaylist(function () {
 
-      audio.parentNode.append(playlist.el);
+      audio.parentNode.append(playlist.el)
 
       audio.addEventListener('canplaythrough', function () {
-        audio.play();
-      }, true);
+        audio.play()
+      }, true)
 
       if(conf.autostart) {
-        player.playTrack(0);
+        player.playTrack(0)
       }
       if(conf.autoplay) {
         audio.addEventListener('ended', function () {
           if (playlist.position < playlist.length) {
-            playlist.position++;
-            audio.setAttribute('src', playlist.current().url);
+            playlist.position++
+            audio.setAttribute('src', playlist.current().url)
           }
-        });
+        })
       }
 
-    });
+    })
   },
   highlight: function (index) {
-    var list = this.playlist.el.children;
+    var list = this.playlist.el.children
     Object.keys(list).forEach(function (li) {
-      list[li].classList.remove('playing');
-    });
-    list[index].classList.add('playing');
+      list[li].classList.remove('playing')
+    })
+    list[index].classList.add('playing')
   },
   playTrack: function (index) {
-    this.position = index;
-    this.audio.setAttribute('src', this.playlist.tracks[index].url);
-    this.highlight(index);
+    this.position = index
+    this.audio.setAttribute('src', this.playlist.tracks[index].url)
+    this.highlight(index)
   },
   playTrackByUrl: function (url) {
     // not yet widely supported
-    //var i = this.playlist.tracks.findIndex(function (x) {return x.url === url;});
+    //var i = this.playlist.tracks.findIndex(function (x) {return x.url === url})
     var i = findIndex(function (x) {
-      return x.url === url;
+      return x.url === url
     },
-    this.playlist.tracks);
+    this.playlist.tracks)
     if (i > -1) {
-      this.playTrack(i);
+      this.playTrack(i)
     }
   },
   loadPlaylist: function (callback) {
-    var player = this;
+    var player = this
     var setUpPlaylist = function (tracks) {
 
-      player.playlist.tracks = tracks;
-      player.playlist.length = tracks.length;
-      player.playlist.position = 0;
+      player.playlist.tracks = tracks
+      player.playlist.length = tracks.length
+      player.playlist.position = 0
 
       for (var i=0; i<tracks.length; i++) {
-        var a  = document.createElement('a');
-        var li = document.createElement('li');
+        var a  = document.createElement('a')
+        var li = document.createElement('li')
 
         a.addEventListener('click', function (ev) {
-          ev.preventDefault();
-          player.playTrackByUrl(ev.target.href);
-        });
-        a.href = tracks[i].url;
+          ev.preventDefault()
+          player.playTrackByUrl(ev.target.href)
+        })
+        a.href = tracks[i].url
         a.append(document.createTextNode(tracks[i].name));
-        li.append(a);
-        player.playlist.el.append(li);
+        li.append(a)
+        player.playlist.el.append(li)
       }
 
-      callback();
-    };
-    xmlHttpGet(this.playlist.url, setUpPlaylist);
+      callback()
+    }
+    xmlHttpGet(this.playlist.url, setUpPlaylist)
   },
-};
+}
